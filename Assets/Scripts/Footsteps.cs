ff3f3139c190405c4fe222756bed22ce4d6dc065
AdriaLama <adria.lama@enti.cat>
2024-05-15 20:03:27 +0200

    push de mi enomre castella

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] footstepsOnCeramic;
+    public AudioClip[] footstepsOnWood;
+
+    public string material;
+
+    void PlayFootstepSound()
     {
-        
+        AudioSource myAudioSource = GetComponent<AudioSource>();
+        myAudioSource.pitch = Random.Range(0.8f, 1.2f);
+        myAudioSource.volume = Random.Range(0.8f, 1.0f);
+
+        switch (material)
+        {
+            case "Ceramica":
+                myAudioSource.PlayOneShot(footstepsOnCeramic[Random.Range(0, footstepsOnCeramic.Length)]);
+                break;
+
+            case "Wood":
+                myAudioSource.PlayOneShot(footstepsOnWood[Random.Range(0, footstepsOnWood.Length)]);
+                break;
+
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        switch (collision.gameObject.tag)
+        {
+            case "Ceramica":
+            case "Wood":
+                material = collision.gameObject.tag;
+                break;
+
+            default:
+                break;
+        }
     }
+
 }
