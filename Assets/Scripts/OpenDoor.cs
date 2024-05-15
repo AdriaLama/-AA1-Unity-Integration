871c02f304ef479d822e36b5f5bee0349dbe4566
MarcReyes99 <146344103+MarcReyes99@users.noreply.github.com>
2024-05-15 18:02:56 +0200

    puerta cruje

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -10,39 +10,61 @@ public class OpenDoor : MonoBehaviour
     public GameObject FirstWindow;
     public GameObject SecondDoor;
     public GameObject SecondWindow;
-    public AudioClip openDoor;
-    public AudioClip closeDoor;
     public AudioSource openDoorSource;
     public AudioSource closeDoorSource;
 
+    public GameObject DoorBasement;
+    public AudioSource DoorSqueak;
+
+
     private Quaternion initialRotationFirstDoor;
     private Quaternion initialRotationSecondDoor;
+    private Quaternion initialRotationDoorBasement;
 
     private void Start()
     {
         initialRotationFirstDoor = FirstDoor.transform.rotation;
         initialRotationSecondDoor = SecondDoor.transform.rotation;
+        initialRotationDoorBasement = DoorBasement.transform.rotation;
     }
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Puerta1"))
         {
             FirstDoor.transform.rotation = initialRotationFirstDoor * Quaternion.Euler(0, -80, 0);
             FirstWindow.SetActive(false);
             SecondDoor.transform.rotation = initialRotationSecondDoor * Quaternion.Euler(0, -80, 0);
             SecondWindow.SetActive(false);
-            openDoorSource.PlayOneShot(openDoor);
+            openDoorSource.Play();
+        }
+        if (collision.gameObject.CompareTag("PuertaSotano"))
+        {
+            StartCoroutine(AbrirPuerta());
         }
     }
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Puerta1"))
         {
             FirstDoor.transform.rotation = initialRotationFirstDoor;
             FirstWindow.SetActive(true);
             SecondDoor.transform.rotation = initialRotationSecondDoor;
             SecondWindow.SetActive(true);
-            closeDoorSource.PlayOneShot(closeDoor);
+            closeDoorSource.Play();
+        }
+    }
+
+    public IEnumerator AbrirPuerta()
+    {
+        if (DoorSqueak != null)
+        {
+            DoorSqueak.Play();
+            for (int i = 0; i < 125; i++)
+            {
+                yield return new WaitForSeconds(0.025f);
+                DoorBasement.transform.rotation = initialRotationDoorBasement * Quaternion.Euler(0, -i, 0);
+            }
+            Destroy(DoorSqueak);
         }
     }
 }
